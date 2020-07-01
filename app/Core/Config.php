dd5f2be17eab3922696b2298e1c8e5fa658a3ed0
Dakpaneel <bradleyoosterveen@gmail.com>
2020-07-01 19:50:35 +0200

    Create config class with load method

---
+++ app/Core/Config.php
@@ -0,0 +1,17 @@
+<?php
+
+
+    namespace Core;
+
+
+    class Config
+    {
+        public static function load()
+        {
+            $ini = parse_ini_file($_SERVER['DOCUMENT_ROOT'].'config.ini', true);
+
+            foreach ($ini as $group => $array)
+                foreach ($array as $key => $value)
+                    define("{$group}_{$key}", "{$value}");
+        }
+    }
