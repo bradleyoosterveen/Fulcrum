5949a3296d08d285e7d27f3e2b94279492448887
Dakpaneel <bradleyoosterveen@gmail.com>
2020-08-11 20:03:12 +0200

    Change paths to CSS and JS files

--- resources/views/app.php
+++ resources/views/app.php
@@ -6,12 +6,12 @@
 
         <title><?=Core\Router::$title?></title>
 
-        <link href="../resources/css/app.css" rel="stylesheet" type="text/css">
+        <link href="css/app.css" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200;400&display=swap" rel="stylesheet">
     </head>
     <body>
         <?php require_once __DIR__.Core\Router::$view; ?>
 
-        <script src="../resources/js/scripts.js"></script>
+        <script src="js/scripts.js"></script>
     </body>
 </html>
