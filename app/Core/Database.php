42c771b35091bbfa87f149c8d54d2533a765ae82
Dakpaneel <bradleyoosterveen@gmail.com>
2020-09-03 22:47:57 +0200

    Add query builder into Database class

--- app/Core/Database.php
+++ app/Core/Database.php
@@ -9,24 +9,46 @@
 
     class Database
     {
+        public $conn;
 
-        /**
-         * @return bool|PDO
-         */
-        public static function construct()
+        private $result;
+        private $query;
+
+        private function __construct() {}
+
+        public static function connect()
         {
             if(empty(SQL_HOST))
                 return false;
 
-            try {
-                $conn = new PDO("mysql:host=" . SQL_HOST . ";dbname=" . SQL_DATABASE, SQL_USER, SQL_PASSWORD);
-                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $db = new Database;
 
-                return $conn;
+            try {
+                $db->conn = new PDO("mysql:host=" . SQL_HOST . ";dbname=" . SQL_DATABASE, SQL_USER, SQL_PASSWORD);
+                $db->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                return $db;
             }
             catch(PDOException $e)
             {
                 return false;
             }
+        }
+
+        function query($query, $params = [])
+        {
+            $q = $this->conn->prepare($query);
+
+            if(!$q->execute())
+                return false;
+
+            $this->result = $q->fetchAll();
+
+            return $this;
+        }
+
+        function result()
+        {
+            return $this->result;
         }
     }
