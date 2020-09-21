88b76e611face2717a2a2c14e4f9686d73f6df39
Bradley Oosterveen <34512356+bradleyoosterveen@users.noreply.github.com>
2020-09-21 23:08:11 +0200

    Fix config.ini sometimes not loading

--- app/Core/Config.php
+++ app/Core/Config.php
@@ -9,7 +9,7 @@
     {
         public static function start()
         {
-            if(!$ini = parse_ini_file($_SERVER['DOCUMENT_ROOT'].'config.ini', true))
+            if(!$ini = parse_ini_file(__DIR__.'/../../config.ini', true))
                 die('Could not find config.ini file. Create one!');
 
             foreach ($ini as $group => $array)
