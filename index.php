935f73ff35a47b1d1ac7f007c27c9a12b8b8bfe1
Dakpaneel <bradleyoosterveen@gmail.com>
2020-09-24 19:43:41 +0200

    Remove hello there

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     /**
      * Use the Composer autoloader
      */
-    require __DIR__.'/../vendor/autoload.php';
+    require __DIR__ . '/../vendor/autoload.php';
 
     /**
      * Include Fulcrum autoloader
