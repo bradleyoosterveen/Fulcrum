fdffd7ea7469c54c08f70c763267ce917b71c903
Dakpaneel <bradleyoosterveen@gmail.com>
2020-09-24 19:44:45 +0200

    Change paths in index.php

--- index.php
+++ index.php
@@ -2,17 +2,17 @@
     /**
      * Use the Composer autoloader
      */
-    require __DIR__ . '/../vendor/autoload.php';
+    require __DIR__ . '/vendor/autoload.php';
 
     /**
      * Include Fulcrum autoloader
      */
-    require __DIR__ . '/../app/Core/Autoloader.php';
+    require __DIR__ . '/app/Core/Autoloader.php';
 
     /**
      * You know what? Let's launch Fulcrum
      */
-    require __DIR__ . '/../app/App.php';
+    require __DIR__ . '/App/App.php';
 
     $app = new Core\App();
 
