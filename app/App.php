4b47124fdb0d05179a3c34c9149fc9b4f22caac7
Dakpaneel <bradleyoosterveen@gmail.com>
2020-10-09 12:55:03 +0200

    Add autoload method to App class

--- app/App.php
+++ app/App.php
@@ -15,12 +15,17 @@
 
         public static function launch()
         {
-            Autoloader::start(); // First and foremost
-
             Session::start();
             Config::start();
             Router::start();
 
             return new App();
+        }
+
+        function autoload() {
+            spl_autoload_register(function ($class) {
+                $class = str_replace('\\', DIRECTORY_SEPARATOR, $class); // Enable cross platform
+                include $_SERVER['DOCUMENT_ROOT'].'app/'.$class.'.php';
+            });
         }
     }
